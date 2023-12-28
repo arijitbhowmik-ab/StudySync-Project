63a50f8a46489ca70935bec901ddf8f2af0441f1
Arijit Bhowmik <122301763+ARIJIT0483@users.noreply.github.com>
2023-12-28 15:35:53 +0530

    scroll-to-top-btn add

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let crosshambtn = document.getElementById('cross-menu-button')
 let responsiveNav = document.getElementById('responsive-header')
 let header = document.getElementsByClassName('header')[0]
 let content = document.getElementsByClassName('content')[0]
+let scrollToTop = document.getElementsByClassName('scroll-top-btn')[0]
 hamButton.addEventListener('click', ()=>{
     responsiveNav.style.display='block'
     header.style.boxShadow='none'
@@ -17,3 +18,10 @@ crosshambtn.addEventListener('click', ()=>{
     hamButton.style.display='block'
     content.style.marginTop='15vh'
 })
+function scrollFunc() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    scrollToTop.style.display='block'
+}
+window.onscroll = function(){
+    scrollFunc()
+}
